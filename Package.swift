710a9d1767147d1b1dccaac37ef18ae561fd0943
Igor Malyarov <igor1309@gmail.com>
2021-02-17 19:08:50 +0300

    WIP

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftToolbox",
-            targets: ["SwiftToolbox"]),
+            targets: ["RegexTools"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftToolbox",
-            dependencies: []),
+            name: "RegexTools",
+            dependencies: []
+        ),
         .testTarget(
-            name: "SwiftToolboxTests",
-            dependencies: ["SwiftToolbox"]),
+            name: "RegexToolsTests",
+            dependencies: ["RegexTools"]
+        ),
     ]
 )
